d294ff92d1957c39c29d1abb9e43545441bd9d26
sbrownium <brownscottbrown@gmail.com>
2023-08-18 17:00:43 -0700

    Test Delete Button

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,10 @@ import App from './App';
 
 test('renders first To Do', () => {
   render(<App />);
-  // const firstTodoText = screen.getByText(/Initial 1/i);
-  // expect(firstTodoText).toBeInTheDocument();
+  const firstTodoText = screen.getByText(/Initial 1/i);
+  expect(firstTodoText).toBeInTheDocument();
   const deleteButton = screen.getAllByText(/delete/i);
-  expect(deleteButton).toBeInTheDocument();
+  expect(deleteButton.length).toBe(3);
 });
 /*
 Are things displayed correctly?
